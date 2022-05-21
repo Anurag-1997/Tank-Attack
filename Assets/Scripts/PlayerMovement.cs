a1b6053827a607604a2455075dc2ec3a0872f4e4
Anurag-1997 <eng.anurag@gmail.com>
2022-05-21 19:39:34 +0530

    Player basic movement done
    Player basic movement has been implemented using new Input System

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    private Rigidbody2D tankRigidbody;
+    [SerializeField] float speed = 5f;
+    PlayerInputActions playerInputActions;
+
+
+
+    private void Awake()
+    {
+        tankRigidbody = GetComponent<Rigidbody2D>();
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+        //playerInputActions.Player.Movement.performed += Movement_performed;
+    }
+    private void Update()
+    {
+        Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
+        //tankRigidbody.AddForce(inputVector * speed, ForceMode2D.Force);
+        this.gameObject.transform.Translate(inputVector.x * speed * Time.deltaTime, inputVector.y * speed * Time.deltaTime, 0);
+    }
+
+    private void Movement_performed(InputAction.CallbackContext context)
+    {
+        Debug.Log("Movement " + context);
+       
+        
+        
+    }
+
+        
+
 }
